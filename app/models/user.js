544b458ac78103ab930acc1e70b6ccba09d77283
Johan Angséus <johan.angseus@gmail.com>
2017-12-06 18:45:34 +0100

    cleaning

--- app/models/user.js
+++ app/models/user.js
@@ -5,9 +5,7 @@ const mongoose = require('mongoose');
 const userSchema = new mongoose.Schema({
   email: { type: String, unique: true },
   password: String,
-
   tokens: Array,
-
   profile: {
     name: String,
     picture: String
